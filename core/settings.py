1aff154a1117d420529096f55806aa99181ba198
Sumit Aggarwal <saggarwal@innow8apps.com>
2024-04-16 12:05:03 +0530

    rest framework installation

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #third party apps
+    'rest_framework',
+
+    # project apps
     'chatbot',
 ]
 
